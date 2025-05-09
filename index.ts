f47fbbf1f73b8ed44c87ee23db087c8f63e3f082
Thalles Leonardelli <thalles63@gmail.com>
2025-05-09 15:14:26 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API REST com TypeScript!");
+});
+
+app.get("/ping", (req, res) => {
+    res.send();
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+// // const express = require("express");
+// const {
+//   exchangeNpssoForAccessCode,
+//   exchangeAccessCodeForAuthTokens,
+// } = require("psn-api");
+// // const app = express();
+
+// async function teste() {
+//   const myNpsso =
+//     "dqYJVKd0Ng8YPAPCEvPGxtoiz1KGh9O76zC62S7DTAC2yS309RSHaeNVRLAXL87W";
+//   const accessCode = await exchangeNpssoForAccessCode(myNpsso);
+//   const authorization = await exchangeAccessCodeForAuthTokens(accessCode);
+
+//   console.log(authorization);
+// }
+
+// teste();
+// // app.get('/api/psn/trophies/:titleId', async (req, res) => {
+// //   try {
+
+// //     // // Autentica e obtém access token (cachê-lo em produção)
+// //     // const { code, access_token } = await authenticate({ npSSO: 'SEU_NPSSO' });
+// //     // // Obtem troféus conquistados para o título especificado
+// //     // const data = await getUserTrophiesEarnedForTitle(
+// //     //   { accessToken: access_token },
+// //     //   'me',
+// //     //   req.params.titleId,
+// //     //   'all'
+// //     // );
+// //     // res.json(data);
+// //   } catch(err) {
+// //     res.status(500).json({ error: err.message });
+// //   }
+// // });
